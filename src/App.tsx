edfec86cd4a41c3b44400ce804cd2409a90fe2c3
Claude Devbox <bojarski.business@gmail.com>
2026-09-01 09:42:51 +0000

    feat: initialisation du projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h3 class="text-red-500"> Hello world.</h3>
+      <h3 className="text-red-500"> Hello world.</h3>
     </>
   )
 }
